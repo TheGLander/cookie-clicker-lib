190c161d49287e34b15bcbd7ca7e36b1525352da
Lubomir <lubomir.yudenko@gmail.com>
2020-02-09 13:54:44 +0200

    Add new injections

--- src/injects.ts
+++ src/injects.ts
@@ -1,9 +1,11 @@
 import { injectCode } from "./helpers"
+import Game from "./gameType"
 
 declare global {
 	interface Window {
-		Game: any
+		Game: Game
 		CCL: any
+		Beautify: Function
 	}
 }
 export function main() {
@@ -16,6 +18,7 @@ export function main() {
 	}
 	let dummy = {}
 	const injections: Array<Injection> = [
+		//Custom menus
 		new Injection("customMenu", [], () => {
 			window.Game.UpdateMenu = injectCode({
 				func: window.Game.UpdateMenu,
@@ -41,7 +44,7 @@ export function main() {
 		new Injection("customOptionsMenu", []),
 		new Injection("customStatsMenu", []),
 		new Injection("customInfoMenu", []),
-		new Injection("customInfoMenu", []),
+		//Data manipulation
 		new Injection("customLoad", [], () => {
 			window.Game.LoadSave = injectCode({
 				func: window.Game.LoadSave,
@@ -50,6 +53,41 @@ export function main() {
 				target: `
 			// CCLib
 			for(const i in CLL.customLoad) CLL.customLoad[i](); `,
+			})
+		}),
+		new Injection("customReset", [], () => {
+			window.Game.Reset = injectCode({
+				func: window.Game.Reset,
+				where: "before",
+				target: `
+			// CCLib
+			for(const i in CCL.customReset) CCL.customReset[i](hard);
+		`,
+			})
+		}),
+		//Misc
+		new Injection("customBeautify", [], () => {
+			window.Beautify = injectCode({
+				func: window.Beautify,
+				source: "return negative?'-'+output:output+decimal;",
+				target: `// CCLib
+  let ret = negative?'-'+output:output+decimal;
+	for(const i in CCL.customBeautify) {
+		let returnedValue = CCL.customBeautify[i](value, floats)
+		ret = returnedValue ? returnedValue : ret
+	};
+	return ret;`,
+				where: "replace",
+			})
+		}),
+
+		new Injection("undefined", undefined, () => {
+			window.Game.Loader.Load = injectCode({
+				func: window.Game.Loader.Load,
+				where: "replace",
+				source: "img.src=this.domain",
+				target:
+					"img.src=(assets[i].indexOf('http') !== -1 ? \"\" : this.domain)",
 			})
 		}),
 	]
